212e1936d17417ed4f781d24eea762f13a5cc867
Jamie G <72987440+JGeddes1@users.noreply.github.com>
2024-07-12 12:33:57 +0100

    3d model messing around

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,8 +8,12 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1}/>
+      <hemisphereLight intensity={3.15} groundColor="black" />
+      <pointLight intensity={10} position={[-0,-1,2.5]}/>
+      <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={10000}
+      castShadow shadow-mapSize={1024} />
+
+      
       <primitive object={computer.scene}
       scale={0.75}
       position={[0, -4.25, -1.5]}
@@ -23,7 +27,7 @@ const ComputersCanvas = () => {
     <Canvas frameloop="demand" shadows camera={{position: [20,3,5], fov:25}}
     gl={{preserveDrawingBuffer: true}}>
 
-      <Suspense >
+      <Suspense fallback={<CanvasLoader />} >
         <OrbitControls enableZoom = {false}
         maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2}/>
         <Computers/>
